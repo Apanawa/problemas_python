d05c363ebf0b03099e714ccd111be026f1c51a45
Adan Gonzalez Flores <108160165+Apanawa@users.noreply.github.com>
2024-05-14 00:33:00 +0000

    Cambio en archivos

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 """
 Description
-Suma dos números
+Escribe un programa que lea un entero entre el 0 y 360 que representa los grados en el
+plano Cartesiano y que muestre como resultado el número del cuadrante en el que se
+encuentra.
 
-Escribe un programa que pida al usuario dos números enteros, y luego muestre en la 
-pantalla su suma.
+En caso de que el grado se encuentre sobre un eje, su programa debe mostrar la palabra
+"axis". En caso de que el grado sea menor que 0 o mayor que 360, su programa debe
+desplegar la palabra "exceeds".
+
+Por ejemplo:
+
+Si la entrada es -10, la salida debe ser exceeds.
 
 Input
-Dos números enteros, uno en cada renglón
+Un entero que representa el número de grados.
 
 Output
-La suma de los dos números tecleados por el usuario
+La palabra quadrant (en minúscula) seguida del número del cuadrante correspondiente
+(por ejemplo: quadrant 2), o alguna de las palabras axis o exceeds.
 
 Example
-Entrada	Salida
-3
-4
-          7
+si el usuario teclea 215
 
+el programa debe mostrar: quadrant 3
+
+Entrada	Salida
+-10     exceeds
+90      axis
+45      quadrant 1
 """
-def suma_numeros():
-          num1 = int(input())
-          num2 = int(input())
-          print(num1 + num2)
+def cuadrante(angulo):
+  if angulo < 0 or angulo > 360:
+    print("exceeds")
+  elif angulo == 90 or angulo == 180 or angulo == 270 or angulo == 0:
+    print("axis")
+  elif angulo > 0 and angulo < 90:
+    print("quadrant 1")
+  elif angulo > 90 and angulo < 180:
+    print("quadrant 4")
+  elif angulo > 180 and angulo < 270:
+    print("quadrant 3")
+  elif angulo > 270 and angulo < 360:
+    print("quadrant 2")
 
 def main():
-          suma_numeros()
+  radio = float(input())
+  cuadrante(radio)
 
 main()
